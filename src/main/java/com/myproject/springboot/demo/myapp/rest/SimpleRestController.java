898163b60da465c81df85597676c108b69678657
dickyarya404 <dickyarya404@gmail.com>
2025-02-19 11:29:52 +0700

    server port spring

--- src/main/java/com/myproject/springboot/demo/myapp/rest/SimpleRestController.java
+++ src/main/java/com/myproject/springboot/demo/myapp/rest/SimpleRestController.java
@@ -1,11 +1,23 @@
 package com.myproject.springboot.demo.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class SimpleRestController {
+
+    // and properties for instrucutr name, and student class
+    @Value("${instructor.name}")
+    private  String instructorName;
+
+    @Value("${student.class}")
+    private String studentClass;
+
+    @GetMapping("classinfo")
+    public String getClassInfo() {
+        return "Instructor: " + instructorName + ", Class: " + studentClass;
+    }
 
     // My Application
 
